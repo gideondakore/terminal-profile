750eb18312d8adf531eeb6be79b32579c8d049d5
gideondakore <dakoregideon72@gmail.com>
2026-04-13 09:25:25 +0000

    Fix: shell compatibility and pip install errors in install_powerline.sh

--- install_powerline.sh
+++ install_powerline.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 # Fail on any command.
-set -eux pipefail
+set -eux -o pipefail
 
 # Install Powerline for VIM.
 sudo apt install -y python3-pip
-pip3 install --user powerline-status
+pip3 install powerline-status
 sudo cp configs/.vimrc ~/.vimrc
 sudo apt install -y fonts-powerline
 
 # Install Patched Font
-mkdir ~/.fonts
+mkdir -p ~/.fonts
 sudo cp -a fonts/. ~/.fonts/
 fc-cache -vf ~/.fonts/
